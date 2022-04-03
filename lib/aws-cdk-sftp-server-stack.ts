255e9d64ce4b6a3fa08233d443df4099e3e5608e
hom-bahrani <hab0184@gmail.com>
2022-04-03 06:25:07 +0100

    Grab config from options

--- lib/aws-cdk-sftp-server-stack.ts
+++ lib/aws-cdk-sftp-server-stack.ts
@@ -1,16 +1,46 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { IHostedZone } from 'aws-cdk-lib/aws-route53';
+import { Certificate, ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
+
+import { options } from './config';
 
 export class AwsCdkSftpServerStack extends Stack {
+  zone?: IHostedZone;
+  certificate?: ICertificate | Certificate;
+
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    
+    const {
+      vpcAttr,
+      sftpAttr,
+      customHostKey,
+      customHostname,
+      users,
+      notificationEmails,
+    } = options;
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkSftpServerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const { customVpcId } = vpcAttr;
+    
+    const {
+      allowCidrs,
+      moveToArchive
+    } = sftpAttr;
+    
+    const {
+      useCustomHostname,
+      dnsAttr,
+      sftpHostname,
+      certificateArn,
+    } = customHostname;
+    
+    const {
+      useCustomKey,
+      hostKeySecretArn,
+      hostKeyVersion
+    } = customHostKey;
+    
   }
 }
